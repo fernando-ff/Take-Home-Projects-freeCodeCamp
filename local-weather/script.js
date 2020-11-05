9110bb6c0a08d34a39d690237acf639c1e556f87
fernando-ff <fferreiralf.1@gmail.com>
2020-11-05 19:14:23 -0300

    feat:initial style

--- local-weather/script.js
+++ local-weather/script.js
@@ -7,6 +7,16 @@ if("geolocation" in navigator)
         fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
         .then(response => response.json())
         .then((data) => {
+            console.log(data)
+            // console.log(data.main);
+            document.getElementById('temp').innerHTML = data.main.temp;
+            document.getElementById('humidity').innerHTML = data.main.humidity;
+            document.getElementById('country').innerHTML = data.sys.country;
+            document.getElementById('city').innerHTML = data.name;
+            document.getElementById('main').innerHTML = data.weather[0].main;
+            document.getElementById('description').innerHTML = data.weather[0].description;
+            document.getElementById('wind-speed').innerHTML = data.wind.speed;
+            document.getElementById('wind-degree').innerHTML = data.wind.deg;
 
         });
     });
