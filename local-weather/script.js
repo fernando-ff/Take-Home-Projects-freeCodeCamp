47e3d6a445608fbde4ef9e343f9333e217e2d94c
fernando-ff <fferreiralf.1@gmail.com>
2020-11-10 18:58:23 -0300

    local weather

--- local-weather/script.js
+++ local-weather/script.js
@@ -1,4 +1,3 @@
-
 if("geolocation" in navigator)
 {
     navigator.geolocation.getCurrentPosition(function({coords}){
@@ -7,17 +6,102 @@ if("geolocation" in navigator)
         fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
         .then(response => response.json())
         .then((data) => {
-            console.log(data)
-            // console.log(data.main);
-            document.getElementById('temp').innerHTML = data.main.temp;
-            document.getElementById('humidity').innerHTML = data.main.humidity;
-            document.getElementById('country').innerHTML = data.sys.country;
-            document.getElementById('city').innerHTML = data.name;
-            document.getElementById('main').innerHTML = data.weather[0].main;
-            document.getElementById('description').innerHTML = data.weather[0].description;
-            document.getElementById('wind-speed').innerHTML = data.wind.speed;
-            document.getElementById('wind-degree').innerHTML = data.wind.deg;
-
+            var temp          =  document.getElementById('temp').innerHTML = data.main.temp + "°C";
+            var humidity      =  document.getElementById('humidity').innerHTML = data.main.humidity + "%";
+            var country       =  document.getElementById('country').innerHTML = data.sys.country;
+            var city          =  document.getElementById('city').innerHTML = data.name;
+            var main          =  document.getElementById('main').innerHTML = data.weather[0].main;
+            var description   =  document.getElementById('description').innerHTML = data.weather[0].description;
+            var wind_speed    = document.getElementById('wind-speed').innerHTML = data.wind.speed + " km/h";
+            var wind_degree   = document.getElementById('wind-degree').innerHTML = data.wind.deg + "°";
+            document.getElementById('temp').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('humidity').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('country').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('city').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('main').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('description').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('wind-speed').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
+              document.getElementById('wind-degree').animate([
+                // keyframes
+                { bottom: '22px',opacity: 0.1 },
+                { bottom: '21px',opacity: 0.25 },
+                { bottom: '20px',opacity: 0.5 },
+                { bottom: '19px',opacity: 0.75 },
+                { bottom: '18px',opacity: 1 }
+              ], { 
+                // timing options
+                duration: 200
+              });
         });
     });
     
